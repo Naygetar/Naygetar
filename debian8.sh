6df22fd849a399a127d5d1d06efe103d82222cd9
Naygetar <31136794+Naygetar@users.noreply.github.com>
2017-08-19 22:16:46 +0700

    Update debian8.sh

--- debian8.sh
+++ debian8.sh
@@ -37,7 +37,7 @@ ln -fs /usr/share/zoneinfo/Asia/Bangkok /etc/localtime
 clear
 echo ""
 echo "Pritunl, Squid Proxy, Firewall .... Install Success ...."
-echo "Debian 8 Jessie"
+echo "Debian 8"
 echo "Source by Naygetar"
 echo ""
 echo "Copy URL and Pless on Browser : https://$MYIP"
