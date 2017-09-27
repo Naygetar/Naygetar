4ed0f6bb487df764bd658902ffbd497554ba527c
Naygetar <31136794+Naygetar@users.noreply.github.com>
2017-09-27 19:36:47 +0700

    Update debian8.sh

--- debian8.sh
+++ debian8.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
-# go to root
-cd
-
 # Install Command
 apt-get -y install ufw
 apt-get -y install sudo
 
 # Install Pritunl
-echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" > /etc/apt/sources.list.d/mongodb-org-3.4.list
-echo "deb http://repo.pritunl.com/stable/apt jessie main" > /etc/apt/sources.list.d/pritunl.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 0C49F3730359A14518585931BC711F9BA15703C6
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-apt-get update
-apt-get --assume-yes install pritunl mongodb-org
-systemctl start mongod pritunl
-systemctl enable mongod pritunl
+sudo tee -a /etc/apt/sources.list.d/mongodb-org-3.4.list << EOF
+deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main
+EOF
+
+sudo tee -a /etc/apt/sources.list.d/pritunl.list << EOF
+deb http://repo.pritunl.com/stable/apt jessie main
+EOF
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 0C49F3730359A14518585931BC711F9BA15703C6
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo apt-get update
+sudo apt-get --assume-yes install pritunl mongodb-org
+sudo systemctl start mongod pritunl
+sudo systemctl enable mongod pritunl
+
+# Install Squid
+apt-get -y install squid3
+cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/Naygetar/Naygetar/master/squid3.conf" 
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
+sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
+service squid3 restart
 
 # Enable Firewall
 sudo ufw allow 22,80,81,222,443,8080,9700,60000/tcp
@@ -23,18 +34,18 @@ sudo ufw allow 22,80,81,222,443,8080,9700,60000/udp
 sudo yes | ufw enable
 
 # Change to Time GMT+7
-ln -fs /usr/share/zoneinfo/Asia/Bangkok /etc/localtime
+ln -fs /usr/share/zoneinfo/Asia/Thailand /etc/localtime
 
 # Install Web Server
 apt-get -y install nginx php5-fpm php5-cli
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/zero9911/pritunl/master/conf/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Naygetar/Naygetar/master/nginx.conf"
 mkdir -p /home/vps/public_html
-echo "<pre>Setup by MKSSHVPN </pre>" > /home/vps/public_html/index.html
+echo "<pre>Sorce by Naygetar</pre>" > /home/vps/public_html/index.html
 echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/zero9911/pritunl/master/conf/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Naygetar/Naygetar/master/vps.conf"
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 service php5-fpm restart
 service nginx restart
@@ -47,7 +58,7 @@ service vnstat restart
 
 # Install Vnstat GUI
 cd /home/vps/public_html/
-wget http://www.sqweek.com/sqweek/files/vnstat_php_frontend-1.5.1.tar.gz
+wget https://github.com/Naygetar/Naygetar/raw/master/vnstat_php_frontend-1.5.1.tar.gz
 tar xf vnstat_php_frontend-1.5.1.tar.gz
 rm vnstat_php_frontend-1.5.1.tar.gz
 mv vnstat_php_frontend-1.5.1 vnstat
@@ -56,21 +67,14 @@ sed -i "s/\$iface_list = array('eth0', 'sixxs');/\$iface_list = array('eth0');/g
 sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
 sed -i 's/Internal/Internet/g' config.php
 sed -i '/SixXS IPv6/d' config.php
-
-# Squid3
-apt-get -y install squid3
-cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
-wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/10p26r/Pritunl/master/squid3.conf"
-MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
-sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
-service squid3 restart
+cd
 
 # About
 clear
 echo ""
 echo "Pritunl .... Install Success ...."
 echo "Debian 8 Jessie"
-echo "Source by Mnm Ami"
+echo "Source by Naygetar"
 echo ""
 echo "Pritunl : https://$MYIP"
 echo "Copy Key and Pless to Pritunl"
